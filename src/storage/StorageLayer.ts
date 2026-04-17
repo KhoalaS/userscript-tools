51c8103b52dec723722933b5e6750a49cb8665ee
KhoalaS <khoalekappa@gmail.com>
2026-04-17 22:50:48 +0200

    make StorageLayer more general

--- src/storage/StorageLayer.ts
+++ src/storage/StorageLayer.ts
@@ -1,8 +1,8 @@
 import { Result } from '../misc/Result'
 
-export interface StorageLayer {
-  add<T>(key: string, value: T, storeName?: string): Promise<Result<unknown>>
-  update<T>(key: string, value: T, storeName?: string): Promise<Result<unknown>>
-  delete(key: string, storeName?: string): Promise<Result<unknown>>
-  get<T>(key: string, storeName?: string): Promise<Result<T>>
+export interface StorageLayer<T> {
+  add(key: string, value: T): Promise<Result<unknown>>
+  update(key: string, value: T): Promise<Result<unknown>>
+  delete(key: string): Promise<Result<unknown>>
+  get(key: string): Promise<Result<T>>
 }
