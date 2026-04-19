157564349efa8d9f3ebd6d0f406febf966c02e46
KhoalaS <khoalekappa@gmail.com>
2026-04-19 19:47:43 +0200

    add leavehandler to routes

--- src/router/SPARouter.ts
+++ src/router/SPARouter.ts
@@ -2,24 +2,28 @@ export type RegexRoute = {
   type: 'regex'
   path: RegExp
   handler: (path: string, ...matches: string[]) => void
+  leaveHandler?: () => void
 }
 
 export type PrefixRoute = {
   type: 'prefix'
   path: string
   handler: (path: string) => void
+  leaveHandler?: () => void
 }
 
 export type ExactRoute = {
   type: 'exact'
   path: string
   handler: (path: string) => void
+  leaveHandler?: () => void
 }
 
 export type Route = ExactRoute | RegexRoute | PrefixRoute
 
 export class SPARouter {
   private routes = new Set<Route>()
+  private lastHandledRoutes: Route[] = []
 
   constructor(routes: Route[]) {
     for (const route of routes) {
@@ -30,28 +34,37 @@ export class SPARouter {
       const parsedUrl = URL.parse(event.destination.url)
       if (!parsedUrl) return
 
+      for (const route of this.lastHandledRoutes) {
+        route.leaveHandler?.()
+      }
+
       const path = parsedUrl.pathname
       this.navigateHandler(path)
     })
   }
 
   private navigateHandler(path: string) {
+    this.lastHandledRoutes = []
+
     for (const route of this.routes.values()) {
       switch (route.type) {
         case 'prefix':
           if (path.startsWith(route.path)) {
             route.handler(path)
+            this.lastHandledRoutes.push(route)
           }
           break
         case 'regex':
           const match = route.path.exec(path)
           if (match) {
             route.handler(path, ...match)
+            this.lastHandledRoutes.push(route)
           }
           break
         case 'exact':
           if (route.path === path) {
             route.handler(path)
+            this.lastHandledRoutes.push(route)
           }
           break
         default:
